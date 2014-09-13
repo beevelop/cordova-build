efdd830122e27fa2829b0bb5713beff7ffe83610
Maik Hummel <info@mh-programming.de>
2014-09-13 19:12:54 +0200

    minor changes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-﻿var $ = require('stringformat')
-var Server = require('./server/Server.js');
-var AgentWorker = require('./agent/AgentWorker.js');
+var $ = require('stringformat')
+var Server       = require('./server/Server.js');
+var AgentWorker  = require('./agent/AgentWorker.js');
 var ClientWorker = require('./client/ClientWorker.js');
 
 module.exports = {
     AgentWorker: AgentWorker,
     ClientWorker: ClientWorker,
-    Server: Server,
+    Server: Server
 };
