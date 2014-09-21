18f18f0fb7f22b21774e0f4d0f6674b75d409f66
Maik Hummel <info@mh-programming.de>
2014-09-21 20:30:21 +0200

    integrate grunt-browserify to rebuild bundle.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,20 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.initConfig({
         jshint: {
             all: ['Gruntfile.js', 'lib/**/*.js']
+        },
+        browserify: {
+            all: {
+                src: "www/js/index.js",
+                dest: "www/js/bundle.js"
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-browserify');
+    
     grunt.registerTask('default', 'jshint');
+    grunt.registerTask('bundle', 'browserify');
 };
--- watch.bat
+++
@@ -1,2 +0,0 @@
-@rem browserify server/public/js/index.js -o server/public/js/bundle.js --debug
-watchify server/public/js/index.js -o server/public/js/bundle.js --debug -v
